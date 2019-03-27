7c0a8a25ada8545464464e8724f7212429314512
unlockingc <624177150@qq.com>
2019-03-27 19:51:18 +0800

    prepare for FPGA finish

---
+++ Examples/normalFPGATest.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./prepareForFPGA.sh testData 4 2 k1 k3
--- Examples/prepareForFPGA.sh
+++ Examples/prepareForFPGA.sh
@@ -3,15 +3,22 @@ if [ ! -d "DeletePart" ]; then
   mkdir DeletePart
 fi
 
+rm DeletePart/*
+
+if [ -d "Coding" ]; then
+  rm Coding -r
+fi
+
 k=$2
 m=$3
 dataPrefix=$1
 partToDelete1=$4
 partToDelete2=$5
-./encoder_single $1 $2 $3 reed_sol_van 8 0 0
 
-mv Coding/${dataPrefix}_${partToDelete1}.txt DeletePart/
-mv Coding/${dataPrefix}_${partToDelete2}.txt DeletePart/
+./encoder $1 $2 $3 reed_sol_van 8 0 0
 
-./decoder_single $1
-./dataPreprocess.py dataPrefix
+mv Coding/${dataPrefix}_${partToDelete1} DeletePart/
+mv Coding/${dataPrefix}_${partToDelete2} DeletePart/
+
+./decoder $1
+./dataPreprocess.py ${dataPrefix}
